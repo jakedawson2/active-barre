1321548f0747650b2e7aef0245bddb081241d869
jake <jake@upbound.com.au>
2022-01-06 11:17:06 +1100

    updated address

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,36 +2,84 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import logo from '../images/logo.png'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span><img src={logo} alt="" /></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Active Barre Studio</h1>
-                <h4>BARRE | PILATES | CARDIO</h4>
-                <h3>5/23 Pulteney Street Taree NSW</h3>
-                <p>Taree's first boutique studio offering a fusion of Barre, Pilates and fitness.</p>
-                
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('studio') }}>Studio</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('work') }}>Classes</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('about') }}>Timetable</a></li>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('contact') }}>Contact</a></li>
-                
-            </ul>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span>
+        <img src={logo} alt="" />
+      </span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Active Barre Studio</h1>
+        <h4>BARRE | PILATES | CARDIO</h4>
+        <h3>Suite 1 216 Victoria Street Taree NSW</h3>
+        <p>
+          Taree's first boutique studio offering a fusion of Barre, Pilates and
+          fitness.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('studio')
+            }}
+          >
+            Studio
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Classes
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            Timetable
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
 }
 
 export default Header
